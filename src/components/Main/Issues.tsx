97f5c769c5a909d0fa6128b8bd3b591e90877728
jxxh204 <ahhancom@gmail.com>
2024-02-02 17:40:37 +0900

    FEAT : route 네이게이터 추가

--- src/components/Main/Issues.tsx
+++ src/components/Main/Issues.tsx
@@ -1,21 +1,32 @@
 import styled from "styled-components";
 import Issue from "./Issue";
 import { IssueType, useIssueState } from "../useContext";
+import { useNavigate } from "react-router-dom";
 
-const IssuesStyle = styled.div`
+const IssuesStyle = styled.ul`
   position: absolute;
   display: flex;
   flex-direction: column;
   gap: 6px;
-  height: 100%;
+  height: 92%;
   overflow-y: auto;
 `;
 function Issues() {
   const state = useIssueState();
+  const navigate = useNavigate();
+  const navigateHandler = (issueNumber: number) => {
+    navigate(`/issue/${issueNumber}`);
+  };
   return (
     <IssuesStyle>
       {state?.map((data: IssueType, index) => (
-        <Issue key={index} data={data} />
+        <Issue
+          onClick={() => {
+            navigateHandler(data.number);
+          }}
+          key={index}
+          data={data}
+        />
       ))}
     </IssuesStyle>
   );
