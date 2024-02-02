f4add4e8e9e693b423edc7c059fa088dca0b67d3
jxxh204 <ahhancom@gmail.com>
2024-02-02 16:56:28 +0900

    FEAT : APP 화면 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,28 @@
 import Main from "components/Main";
-import Issues from "./components/Main/Issues";
-import Bottom from "./components/Bottom";
-import { CurrentIssueProvider } from "./components/useContext";
-import { useEffect } from "react";
+import Issues from "components/Main/Issues";
+import Bottom from "components/Bottom";
+import { CurrentIssueProvider } from "components/useContext";
+import Title from "components/Title";
+import styled from "styled-components";
+
+const AppStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 function App() {
   //제출전에 토큰 빼야함.
-  const authToken =
-    "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";
-  useEffect(() => {
-    fetch(" https://api.github.com/repos/angular/angular-cli/issues", {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${authToken}`,
-        "Content-Type": "application/json",
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => data.map((issue) => console.log(issue)));
-  }, []);
+
   return (
     <CurrentIssueProvider>
-      <Main>
-        <Issues issues={null} />
-      </Main>
-      <Bottom load={null} reset={null} />
+      <AppStyle>
+        <Title>Anguler / Angular-cli</Title>
+        <Main>
+          <Issues />
+        </Main>
+        <Bottom />
+      </AppStyle>
     </CurrentIssueProvider>
   );
 }
