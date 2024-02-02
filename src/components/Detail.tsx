a6457170f94d81bf6a7d0342d4b85e8ea43090f0
jxxh204 <ahhancom@gmail.com>
2024-02-02 17:56:25 +0900

    FEAT : 디테일 페이지 데이터 컨텍스트 작성

--- src/components/Detail.tsx
+++ src/components/Detail.tsx
@@ -1,5 +1,16 @@
+import styled from "styled-components";
+import { useIssueState } from "./useContext";
+
+const DetailStyle = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
 function Detail() {
-  return <>Detail</>;
+  const state = useIssueState();
+  state;
+  return <DetailStyle>Detail</DetailStyle>;
 }
 
 export default Detail;
--- src/components/useContext.tsx
+++ src/components/useContext.tsx
@@ -23,13 +23,21 @@ const IssueContext = createContext<IssueType[]>([]);
 const IssueDispatchContext = createContext<null | Dispatch<
   SetStateAction<IssueType[]>
 >>(null);
+const GetDetailIssue = createContext<
+  null | ((issueNumber: number) => IssueType[])
+>(null);
 
 function CurrentIssueProvider({ children }: { children: React.ReactNode }) {
   const [issue, setIssue] = useState<IssueType[]>([]);
+  const getDetailHandler = (issueNumber: number) => {
+    return issue.filter((i) => i.number === issueNumber);
+  };
   return (
     <IssueContext.Provider value={issue}>
       <IssueDispatchContext.Provider value={setIssue}>
-        {children}
+        <GetDetailIssue.Provider value={getDetailHandler}>
+          {children}
+        </GetDetailIssue.Provider>
       </IssueDispatchContext.Provider>
     </IssueContext.Provider>
   );
@@ -49,5 +57,17 @@ function useIssueDispatch() {
   }
   return context;
 }
+function useGetDetailIssue() {
+  const context = useContext(IssueDispatchContext);
+  if (context === undefined) {
+    throw new Error("이슈 컨텍스트 없음.");
+  }
+  return context;
+}
 
-export { CurrentIssueProvider, useIssueState, useIssueDispatch };
+export {
+  CurrentIssueProvider,
+  useIssueState,
+  useIssueDispatch,
+  useGetDetailIssue,
+};
